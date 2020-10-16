713330795544972675ef5ea55a31ff6ceecb3dd8
rakslice <rakslice@gmail.com>
2020-10-16 02:22:47 -0700

    windows: remove _UNICODE from sysdeps; change make links to work with files, not just directories

--- BasiliskII/src/Windows/sysdeps.h
+++ BasiliskII/src/Windows/sysdeps.h
@@ -21,10 +21,6 @@
 #ifndef SYSDEPS_H
 #define SYSDEPS_H
 
-#ifdef __MINGW32__
-#define _UNICODE
-#endif
-
 #if !defined _MSC_VER && !defined __STDC__
 #error "Your compiler is not ANSI. Get a real one."
 #endif
